e0c34a088442d568e3c2f52f9943f9569b25847e
Dzankomelli <timi.giaco@gmail.com>
2018-12-18 18:02:37 +0100

    osnovne sole

--- uvoz/uvoz_test1.R
+++ uvoz/uvoz_test1.R
@@ -4,5 +4,8 @@ library(tidyr)
 
 
 #1. tabela
-osnovne_sole <- read.csv2('../podatki/osnovne_sole.csv')
+osnovne_sole <- read.csv2(file = 'podatki/osnovnesole.csv', skip = 3,
+                          header = FALSE, encoding = 'Windows-1250', sep = ';', dec = '.',
+                          col.names = c('Leto', 'brezveze', 'Regija', 'Spol', 'Vrednost'))
 
+osnovne_sole <- osnovne_sole[,-2]

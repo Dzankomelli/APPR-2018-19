a02357ef653850e46369377cbbb5ffcf7bd5a249
Dzankomelli <timi.giaco@gmail.com>
2018-12-20 08:34:43 +0100

    Update libraries.r

--- lib/libraries.r
+++ lib/libraries.r
@@ -3,6 +3,9 @@ library(rvest)
 library(gsubfn)
 library(reshape2)
 library(shiny)
-
+library(readr)
+library(dplyr)
+library(rmarkdown)
+library(tidyr)
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r", encoding="UTF-8")

8ac393fe4f6200318d1177fb5816482532054781
Dzankomelli <timi.giaco@gmail.com>
2019-01-05 23:20:11 +0100

    Update vizualizacija_Timotej.R

--- vizualizacija/vizualizacija_Timotej.R
+++ vizualizacija/vizualizacija_Timotej.R
@@ -1,8 +1,20 @@
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r", encoding="UTF-8")
 
-map <- get_map(location = 'Slovenia', zoom = 4)
-ggmap(map)
+#ZEMLJEVID
+library(sp)
+library(maptools)
+library(digest)
+gpclibPermit()
+
+zemljevid <- uvozi.zemljevid("http://biogeo.ucdavis.edu/data/gadm2.8/shp/SVN_adm_shp.zip",
+                             "SVN_adm1", encoding = "UTF-8") %>% pretvori.zemljevid()
+
+
+levels(zemljevid$NAME_1)[levels(zemljevid$NAME_1) %in%
+                           c("Notranjsko-kraška",
+                             "Spodnjeposavska", "Koroška", "Goriška", "Obalno-kraška")] <- c("Primorsko-notranjska",
+                                                                                             "Posavska", "Koroska", "Goriska", "Obalno-kraska")
 
 
 

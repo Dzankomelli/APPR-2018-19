2519064f620a6657c9daf819cee5b85ec3ed901e
Dzankomelli <timi.giaco@gmail.com>
2019-01-05 15:24:51 +0100

    Update vizualizacija_Timotej.R

--- vizualizacija/vizualizacija_Timotej.R
+++ vizualizacija/vizualizacija_Timotej.R
@@ -1,2 +1,5 @@
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r", encoding="UTF-8")
+
+graf_bruto_proizvod <- ggplot((data = bruto), aes(x=Leto, y=BDP, col=Regija)) + geom_point()
+plot(graf_bruto_proizvod)

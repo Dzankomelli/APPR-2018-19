f9ad56042807165efea6c61aa6aaacfd3be6eade
Dzankomelli <timi.giaco@gmail.com>
2019-01-06 18:57:37 +0100

    Update vizualizacija_Timotej.R

--- vizualizacija/vizualizacija_Timotej.R
+++ vizualizacija/vizualizacija_Timotej.R
@@ -2,23 +2,13 @@
 source("lib/uvozi.zemljevid.r", encoding="UTF-8")
 
 
-# Uvozimo zemljevid Sveta
-# source("https://raw.githubusercontent.com/jaanos/APPR-2018-19/master/lib/uvozi.zemljevid.r")
-source("lib/uvozi.zemljevid.r") #Nastavi pravo datoteko
-
-zemljevid <- uvozi.zemljevid("https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip",
-                             "ne_50m_admin_0_countries") %>%
-  fortify()
-
-
-# Zemljevid sveta skrčimo na zemljevid Evrope
-Evropa <- filter(zemljevid, CONTINENT == "Europe" |NAME == "Turkey")
-Evropa <- filter(Evropa, long < 55 & long > -45 & lat > 30 & lat < 85)
-
-ggplot(Evropa, aes(x=long, y=lat, group=group, fill=NAME)) + 
-  geom_polygon() + 
-  labs(title="Evropa - osnovna slika") +
-  theme(legend.position="none")
+# Uvozimo zemljevid.
+zemljevid <- uvozi.zemljevid("https://gadm.org/maps/SVN_1.html.zip", "OB",
+                             pot.zemljevida="OB", encoding="Windows-1250")
+levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
+{ gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
+zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels=levels(obcine$obcina))
+zemljevid <- fortify(zemljevid)
 
 
 

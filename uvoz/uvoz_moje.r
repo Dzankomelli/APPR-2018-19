9fe8384dc2a44be5a649973f68e09a8e3f4c40e0
Dzankomelli <timi.giaco@gmail.com>
2019-05-05 18:33:57 +0200

    Update uvoz_moje.r

--- uvoz/uvoz_moje.r
+++ uvoz/uvoz_moje.r
@@ -6,7 +6,7 @@ library(tidyr)
 
 #1. tabela
 osnovne_sole <- read.csv2(file = 'podatki/osnovnesole.csv', skip = 3, na = c(' ','-'),
-                          header = FALSE, encoding = 'Windows-1250', sep = ';', dec = '.',
+                          header = FALSE, fileEncoding = 'Windows-1250', sep = ';', dec = '.',
                           col.names = c('Leto', 'brezveze', 'Regija', 'Spol', 'Stevilo'))
 
 osnovne_sole <- osnovne_sole[,-2]
@@ -38,7 +38,7 @@ diplomanti$Leto <- as.integer(diplomanti$Leto)
 
 #3. tabela
 vrtec <- read.csv2(file = 'podatki/vrtec.csv', skip = 1, header = FALSE, 
-                   encoding = 'Windows-1250', sep = ';', na = c(' ','-'),
+                   fileEncoding = 'Windows-1250', sep = ';', na = c(' ','-'),
                    col.names = c('Leto', 'Regija', 'Spol', 'Stevilo'))
 
 vrtec <- vrtec %>% fill(1:2)%>% filter(complete.cases(.))
@@ -67,7 +67,7 @@ vrtec_sum$Leto <- as.integer(vrtec_sum$Leto)
 
 #4. tabela
 dijaki <- read.csv2(file = 'podatki/dijaki.csv', skip = 3, header = FALSE, 
-                    encoding = 'Windows-1250', sep = ';', na = c(' ','-'),
+                    fileEncoding = 'Windows-1250', sep = ';', na = c(' ','-'),
                     col.names = c('Leto', 'Regija', 'krneki', 'Stevilo'))
 
 dijaki <- dijaki[,-3]
@@ -80,7 +80,7 @@ dijaki <- dijaki %>% filter(Regija != 'SLOVENIJA')
 
 #5. tabela
 terciarno <- read.csv2(file = 'podatki/terciarno.csv', skip = 2, header = FALSE,
-                       encoding = 'Windows-1250', sep = ';', na = c(' ','-'),
+                       fileEncoding = 'Windows-1250', sep = ';', na = c(' ','-'),
                        col.names = c('Leto', 'Regija', 'Vrsta', 'Spol', 'Stevilo'))
 
 terciarno <- terciarno %>% fill(1:3)%>% filter(complete.cases(.))
@@ -109,7 +109,7 @@ terciarno_sum$Leto <- as.integer(terciarno_sum$Leto)
 
 #6. tabela
 bruto <- read.csv2(file = 'podatki/bruto_proizvod.csv', skip = 0, header = FALSE,
-                   encoding = 'Windows-1250', sep = ';', na = c(' ','-'),
+                   fileEncoding = 'Windows-1250', sep = ';', na = c(' ','-'),
                    col.names = c('Leto', 'Regija', 'Krneki', 'BDP'))
 
 
@@ -134,7 +134,7 @@ bruto <- bruto[,-3]
 #7. tabela
 
 prebivalstvo <- read.csv(file = 'podatki/prebivalci.csv', skip = 3, header = FALSE,
-                          encoding = 'Windows-1250', sep = ';', na = c(' ','-'), nrows = 170,
+                          fileEncoding = 'Windows-1250', sep = ';', na = c(' ','-'), nrows = 170,
                           col.names = c('Regija', 'Leto', 'Stevilo'))
 
 

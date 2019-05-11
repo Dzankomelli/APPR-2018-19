cc2ae4b08a7a6d84406a63adc53554718c0ba0fd
Dzankomelli <timi.giaco@gmail.com>
2019-05-11 10:31:49 +0200

    Update analiza.r

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -8,7 +8,6 @@ bruto_slovenija1$BDP <- as.character(bruto_slovenija1$BDP)
 bruto_slovenija1 <- bruto_slovenija1 %>% mutate(Leto = parse_integer(Leto), BDP = parse_number(BDP))
 prileganje <- lm(BDP ~ Leto, data = bruto_slovenija1)
 predict(prileganje, data.frame(Leto=seq(2017,2020,1)))
-napoved <- mutate(data.frame(Leto=seq(2017,2020,1)), bruto_slovenija1 = predict(prileganje,newdata=data.frame(Leto=seq(2017,2020,1))))
 
 
 graf_napoved <- ggplot(bruto_slovenija1, aes(x=Leto, y=BDP)) + geom_point() + 
